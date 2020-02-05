01a128d68c1a25fdf00427b86a5e351e6eeb93df
reshetnikd <dmitry.reshetnik@icloud.com>
2020-02-05 22:54:41 +0200

    Try out some code

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -10,12 +10,82 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+//        VStack {
+////            Image("Example")
+////                .resizable()
+////                .aspectRatio(contentMode: .fill)
+////                .frame(width: 300, height: 300)
+////               .clipped()
+//            GeometryReader { geo in
+//                Image("Example")
+//                    .resizable()
+//                    .aspectRatio(contentMode: .fit)
+//                    .frame(width: geo.size.width)
+//            }
+//
+//            ScrollView(.vertical) {
+//                VStack(spacing: 10) {
+//                    ForEach(0..<100) {
+//                        CustomText("Item \($0)")
+//                            .font(.title)
+//                    }
+//                }
+//                .frame(maxWidth: .infinity)
+//            }
+//        }
+//        NavigationView {
+//            List(0..<100) { row in
+//                NavigationLink(destination: Text("Detailed \(row)")) {
+//                    Text("Row \(row)")
+//                }
+//            }
+//            .navigationBarTitle("SwiftUI")
+//        }
+        Button("Decode JSON") {
+            let input = """
+            {
+                "name": "Taylor Swift",
+                "address": {
+                    "street": "555, Taylor Swift Avenue",
+                    "city": "Nashville"
+                }
+            }
+            """
+            
+            let data = Data(input.utf8)
+            let decoder = JSONDecoder()
+            if let user = try? decoder.decode(User.self, from: data) {
+                print(user.address.street)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct User: Codable {
+    var name: String
+    var address: Address
+}
+
+struct Address: Codable {
+    var street: String
+    var city: String
+}
+
+struct CustomText: View {
+    var text: String
+    
+    var body: some View {
+        Text(text)
+    }
+    
+    init(_ text: String) {
+        print("Creating a new CustomText")
+        self.text = text
     }
 }
