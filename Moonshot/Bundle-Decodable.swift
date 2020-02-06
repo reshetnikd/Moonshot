8a06a8741e9f6a706b240c4bcb1a419c4d6a4d3d
reshetnikd <dmitry.reshetnik@icloud.com>
2020-02-06 19:25:07 +0200

    Add supporting files and extension to decode JSON

---
+++ Moonshot/Bundle-Decodable.swift
@@ -0,0 +1,29 @@
+//
+//  Bundle-Decodable.swift
+//  Moonshot
+//
+//  Created by Dmitry Reshetnik on 06.02.2020.
+//  Copyright © 2020 Dmitry Reshetnik. All rights reserved.
+//
+
+import Foundation
+
+extension Bundle {
+    func decode(_ file: String) -> [Astronaut] {
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Failed to locate \(file) in bundle.")
+        }
+        
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to load \(file) from bundle.")
+        }
+        
+        let decoder = JSONDecoder()
+        
+        guard let loaded = try? decoder.decode([Astronaut].self, from: data) else {
+            fatalError("Failed to decode \(file) from bundle.")
+        }
+        
+        return loaded
+    }
+}
