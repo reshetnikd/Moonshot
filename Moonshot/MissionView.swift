8a8003f7c153ee90815a4a1590d1390b69384686
reshetnikd <dmitry.reshetnik@icloud.com>
2020-02-07 18:32:35 +0200

    Add MissionView

---
+++ Moonshot/MissionView.swift
@@ -0,0 +1,41 @@
+//
+//  MissionView.swift
+//  Moonshot
+//
+//  Created by Dmitry Reshetnik on 07.02.2020.
+//  Copyright © 2020 Dmitry Reshetnik. All rights reserved.
+//
+
+import SwiftUI
+
+struct MissionView: View {
+    let mission: Mission
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ScrollView(.vertical) {
+                VStack {
+                    Image(self.mission.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(maxWidth: geometry.size.width * 0.7)
+                        .padding(.top)
+                    
+                    Text(self.mission.description)
+                        .padding()
+                    
+                    Spacer(minLength: 25)
+                }
+            }
+        }
+        .navigationBarTitle(Text(mission.displayName), displayMode: .inline)
+    }
+}
+
+struct MissionView_Previews: PreviewProvider {
+    static let missions: [Mission] = Bundle.main.decode("missions.json")
+    
+    static var previews: some View {
+        MissionView(mission: missions[0])
+    }
+}
